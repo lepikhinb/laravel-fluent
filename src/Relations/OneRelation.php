a569cb8e40cdd8b816d345a732c740e4d12f6416
Boris Lepikhin <lepikhinb@gmail.com>
2021-09-05 22:32:24 +0300

    wip

--- src/Relations/OneRelation.php
+++ src/Relations/OneRelation.php
@@ -2,9 +2,6 @@
 
 namespace Based\Fluent\Relations;
 
-use Attribute;
-
-#[Attribute(Attribute::TARGET_CLASS|Attribute::TARGET_PROPERTY)]
 abstract class OneRelation extends AbstractRelation
 {
     public function __construct()
